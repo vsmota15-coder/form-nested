7ac421a07a0097d68cd6ed2b8c80db02f4ebc1aa
vsmota15-coder <vsmota15@gmail.com>
2026-01-19 22:55:52 -0300

    docker-compose.yml adicionado e banco de dados atualizado

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2025_12_20_184355) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "addresses", force: :cascade do |t|
     t.string "city"
     t.datetime "created_at", null: false
